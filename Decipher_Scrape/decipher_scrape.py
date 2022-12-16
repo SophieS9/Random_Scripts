d71d86364498c21f913f4ad5ff07973a87a00a10
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2022-12-16 16:45:05 +0000

    Scraping patient variants

--- Decipher_Scrape/decipher_scrape.py
+++ Decipher_Scrape/decipher_scrape.py
@@ -12,6 +12,7 @@ from selenium.webdriver.chrome.service import Service
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import re
 
 ###############################
 ###        Functions        ###
@@ -25,15 +26,119 @@ def get_patient_variants(gene):
 	#Empty list to store variants
 	patient_variants=[]
 
-	#Get URL for patient variants for gene
-	URL = "https://www.deciphergenomics.org/gene/"+gene+"/patient-overlap/snvs"
-	page = requests.get(URL)
+	#Get URL for patient variants for gene - had to use selenium to run the js to get all info
+	chrome_options = Options()
+	chrome_options.add_argument("--no-sandbox")
+	chrome_options.add_experimental_option("detach", True)
+	chrome_options.add_argument("--headless")
+
+	webdriver_service = Service("./chromedriver") #Your chromedriver path
+	driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+	url = "https://www.deciphergenomics.org/gene/"+gene+"/patient-overlap/snvs"
+	driver.get(url)
+	driver.maximize_window()
+	time.sleep(5)
 
 	#Use beautiful soup to parse the html
-	soup = BeautifulSoup(page.content, "html.parser")
+	soup = BeautifulSoup(driver.page_source, "html.parser")
+	
+	#Need to get some info on the gene to limit variants to only those in GOI
+	full_info = soup.h1.small.contents[0]
+	check_chr = full_info.split(":")[0]
+	check_start = full_info.split(":")[1].split("-")[0]
+	check_end = full_info.split(":")[1].split("-")[1]
 
-#	print(soup.body.div.prettify())
+	#Get all the patient identifiers and will then link out to their variant pages
+	patient_ids = []
+	
+	for tag in soup.find_all('td'):
+		
+		#Get patient ID from link - this is all sub tags called div which have a lcass matching btn-group-xs
+		if tag.div is not None and tag.div.has_attr('class'):
+			if 'btn-group-xs' in tag.div['class']:
+				
+				#just get out what I need!
+				full_line = str(tag.div.contents[0])
+				patient_search = re.search('patient\/(.+?)\/contact', full_line)
+				patient = patient_search.group(1)
+				
+				#Add to patient list 
+				patient_ids.append(patient)
+	
+	#Can now get the variant info from the patient pages! 
+	for patient in patient_ids:
+		
+		#Do another scrape
+		#Get URL for patient 
+		chrome_options = Options()
+		chrome_options.add_argument("--no-sandbox")	
+		chrome_options.add_experimental_option("detach", True)
+		chrome_options.add_argument("--headless")
 
+		webdriver_service = Service("./chromedriver") #Your chromedriver path
+		driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+		url = "https://www.deciphergenomics.org/patient/"+patient+"/genotype/"
+		driver.get(url)
+		driver.maximize_window()
+		time.sleep(5)
+
+		#Use beautiful soup to parse the html
+		patient_soup = BeautifulSoup(driver.page_source, "html.parser")
+		
+		#Markers to use to bring the variant information together as they're in sequential tags in the html
+		got_chr = False
+		got_pos = False
+		got_ref = False
+		got_alt = False
+		
+		#The information we want is within span tags, specifically those with classes
+		for tag in patient_soup.find_all('span'):
+	
+			#If it has a class label
+			if tag.has_attr('class'):
+				
+				#Chromosome
+				if 'chr' in tag['class']:
+					chrm = tag.contents[0]
+					#Only change marker if chromosome matches
+					if chrm == check_chr:
+						got_chr = True
+				
+				#Position
+				if 'start' in tag['class']:
+					position = tag.contents[0]
+					#Only change if position is between start and stop
+					if position >= check_start and position <= check_end:
+						got_pos = True
+				
+				#Ref - this might be a list if multiple bases, so need to loop
+				if 'allele-ref' in tag['class']:
+					ref = ''
+					for base in tag.contents:
+						#Only take the bases
+						if 'allele-replace' not in base['class']:
+							ref += base.contents[0]
+					got_ref = True
+				
+				#Alt - as above, might be a list
+				if 'allele-alt' in tag['class']:
+					alt = ''
+					for base in tag.contents:
+						alt += base.contents[0]
+					got_alt = True
+			
+				#Put together into a variant when all elements got, then reset
+				if got_chr and got_pos and got_ref and got_alt:
+				
+					var = chrm+":"+position+ref+">"+alt
+				
+					got_chr = False
+					got_pos = False
+					got_ref = False
+					got_alt = False
+				
+					patient_variants.append(var)
+		
 	return(patient_variants)
 
 
@@ -136,3 +241,5 @@ with open(args.out,'w') as out:
 	for var in research_variants:
 		out.write(var+"\n")
 		
+	for var in patient_variants:
+		out.write(var+"\n")
